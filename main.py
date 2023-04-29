cfc72459f5c1fd80577f014b62351b144f8528c6
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-04-29 16:32:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ print("Hi Vishali")
 def Welcome():
     print("Welcome to GITHUB Learning")
 
-print("This is sample test for GITHUB Actions")
+print("This is sample test for GITHUB Actions...!")
 Welcome()
